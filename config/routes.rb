4feed79db95c3385ef493f41fe6526fe6d5a3863
Marco Vittini <mvittini0@gmail.com>
2020-08-02 21:41:17 -0400

    create report controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
 end
+Rails.application.routes.draw do
+  match '/admin/reports/foo_report' => 'admin/reports#foo_report', :via => [:get, :post]
+end
